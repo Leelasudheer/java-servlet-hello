a127244fd4da11ae2ed2790d7e5fa0afe25bcab0
Leelasudheer <80817777+Leelasudheer@users.noreply.github.com>
2022-07-15 11:20:56 +0530

    update
    update

--- src/main/java/com/mycompany/hello/HelloServlet.java
+++ src/main/java/com/mycompany/hello/HelloServlet.java
@@ -9,6 +9,7 @@ import java.io.PrintWriter;
 public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<HTML>");
         out.println("<HEAD>");
