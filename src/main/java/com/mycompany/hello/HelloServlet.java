7e774f7377bdfee065f16c268cf203ecfa52378c
Leelasudheer <80817777+Leelasudheer@users.noreply.github.com>
2022-07-15 11:42:08 +0530

    Update HelloServlet.java

--- src/main/java/com/mycompany/hello/HelloServlet.java
+++ src/main/java/com/mycompany/hello/HelloServlet.java
@@ -1,23 +1,32 @@
 package com.mycompany.hello;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+// Import required java libraries
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
+// Extend HttpServlet class
 public class HelloServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request,
-                      HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<HTML>");
-        out.println("<HEAD>");
-        out.println("<TITLE>Hello Servlet</TITLE>");
-        out.println("</HEAD>");
-        out.println("<BODY>");
-        out.println("Hello Servlet");
-        out.println("</BODY>");
-        out.println("</HTML>");
-    }
+ 
+   private String message;
+
+   public void init() throws ServletException {
+      // Do required initialization
+      message = "Hello my name is Sreeni";
+   }
+
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+      
+      // Set response content type
+      response.setContentType("text/html");
+
+      // Actual logic goes here.
+      PrintWriter out = response.getWriter();
+      out.println("<h1>" + message + "</h1>");
+   }
+
+   public void destroy() {
+      // do nothing.
+   }
 }
